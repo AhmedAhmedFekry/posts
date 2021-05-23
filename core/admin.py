af614c0b243c90ce73a3296a9fb2a187b06a9d6a
AhmedAhmedFekry <ahmedahmedfekry11305654@gmail.com>
2021-05-23 09:03:22 +0200

    import and export in admin

--- core/admin.py
+++ core/admin.py
@@ -1,18 +1,24 @@
 from django.contrib import admin
 from .models import Post, Comment, Like, Category
+from import_export.admin import ImportExportModelAdmin
+
 # Register your models here.
 
 
-class LikeAdmin(admin.ModelAdmin):
+class CommentAdmin(ImportExportModelAdmin):
+    list_display = ['comment', 'post', "commenter", ]
+
+
+class LikeAdmin(ImportExportModelAdmin):
     list_display = ['comment', 'user']
 
 
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(ImportExportModelAdmin):
     list_display = ['title', 'status']
     prepopulated_fields = {'slug': ('title',)}
 
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(ImportExportModelAdmin):
     list_display = ['title', 'category', 'create_at']
     date_hierarchy = 'create_at'
     # raw_id_fields = ('category',)
@@ -21,5 +27,5 @@ class PostAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Like, LikeAdmin)
